d4b5e396913aeb0b94f439348a199ae6612f219b
ParnelJ <john.parnell@ecclesiastical.com>
2022-02-05 22:53:08 +0000

    Further work

--- src/ArraySortDetermine.java
+++ src/ArraySortDetermine.java
@@ -1,5 +1,16 @@
+
+
+
+
 public class ArraySortDetermine {
 
+public static boolean solution(in[] n, int N) {
+
+    if (N <  0){
+        throw new UnsupportedOperationException("Argument N cannot be less than 0");
+
+    }
+}
 
 
 }
